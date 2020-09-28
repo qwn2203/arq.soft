280bfa9d5ac6b2eb2e5181f61c9ff768e526f127
Alma Ayaquica <yogo060@gmail.com>
2020-09-28 16:59:12 -0500

    sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  {path: '/home', title: 'Home', icon: 'home', class: ''},
+  {path: '/login', title: 'LogIn', icon: 'login', class: ''},
+]
 
 @Component({
   selector: 'app-root',
@@ -7,6 +18,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  menuItems: any[];
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
 }
 
 
