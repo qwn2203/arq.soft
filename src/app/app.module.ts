2a6f877cb40cbbdf54d33e5be3ef3efc849dcf5b
JavierPinedoC <a01551344@itesm.mx>
2020-09-29 18:46:27 -0500

    se agregaron los dos nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { InstructionsComponent } from './components/instructions/instructions.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeAppComponent } from './components/home-app/home-app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    InstructionsComponent
+    InstructionsComponent,
+    NavbarComponent,
+    HomeAppComponent
   ],
   imports: [
     BrowserModule,
