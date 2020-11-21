d34b01e08a48b21967f2bb269b6f873a99b2be98
JavierPinedoC <a01551344@itesm.mx>
2020-11-20 21:30:13 -0600

    componente calificaciones

---
+++ src/app/components/calificaciones/calificaciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calificaciones',
+  templateUrl: './calificaciones.component.html',
+  styleUrls: ['./calificaciones.component.css']
+})
+export class CalificacionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
