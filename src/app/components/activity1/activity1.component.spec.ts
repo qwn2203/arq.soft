dd692bf068366e25b498b1fca56ec6768b2d1d22
JavierPinedoC <a01551344@itesm.mx>
2020-11-20 23:16:34 -0600

    falta validación de datos y corregir bugs

--- src/app/components/activity1/activity1.component.spec.ts
+++ src/app/components/activity1/activity1.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { Activity1Component } from './activity1.component';
 
@@ -8,7 +11,8 @@ describe('Activity1Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ Activity1Component ]
+      declarations: [ Activity1Component ],
+      imports: [ReactiveFormsModule, HttpClientModule, RouterTestingModule],
     })
     .compileComponents();
   });
--- src/app/components/activity1/activity1.component.ts
+++ src/app/components/activity1/activity1.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataApiService } from 'src/app/services/data-api.service';
 
 @Component({
   selector: 'app-activity1',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity1.component.css']
 })
 export class Activity1Component implements OnInit {
-
-  constructor() { }
+  forma: FormGroup;
+  constructor(private fb: FormBuilder,private apiservice: DataApiService, private router: Router) { 
+    this.preguntas();
+  }
 
   ngOnInit(): void {
   }
 
+  preguntas(){
+    this.forma = this.fb.group({
+      q1: ['',, ],
+      q2: ['',, ],
+      q3: ['', []],
+      q4t: ['',, ],
+      q4f: ['',, ],
+    })
+  }
+
+  guardar(){
+    console.log(this.forma);
+    
+  }
 }
