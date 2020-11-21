3a8980e71768fe293ab542b263a22a5ed80ee1d4
JavierPinedoC <a01551344@itesm.mx>
2020-11-20 21:56:38 -0600

    registro

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataApiService } from 'src/app/services/data-api.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  forma: FormGroup;
+  constructor(private fb: FormBuilder,private apiservice: DataApiService, private router: Router) { }
 
   ngOnInit(): void {
+    this.forma = this.fb.group({
+      name: ['',Validators.required, ],
+      lastname: ['',Validators.required, ],
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
+      password: ['',Validators.required, ],
+    })
+  }
+
+  guardar(){
+    
   }
 
 }
