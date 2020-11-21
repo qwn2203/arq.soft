5563dbced43758ee1a074760be6809aae9d8af6e
JavierPinedoC <a01551344@itesm.mx>
2020-11-20 22:04:27 -0600

    agregar dependencia

--- src/app/components/signup/signup.component.spec.ts
+++ src/app/components/signup/signup.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SignupComponent } from './signup.component';
 
@@ -8,7 +11,8 @@ describe('SignupComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
+      declarations: [ SignupComponent ],
+      imports: [ReactiveFormsModule, HttpClientModule, RouterTestingModule],
     })
     .compileComponents();
   });
